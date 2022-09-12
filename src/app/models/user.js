e88bf8a3bd82a6bf814f4d7d0b2f5b36d39e7937
tuantm0303 <minhtuan0330@gmail.com>
2022-09-12 16:49:10 +0700

    feat: user model

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,7 +1,45 @@
 import mongoose, { Schema } from "mongoose";
-import { createHmac } from "crypto";
+import bcrypt from "bcrypt";
 
-const userSchema = new Schema({
-  name: { type: String },
-  email: { type: String },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
+
+userSchema.methods.isValidPassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default mongoose.model("User", userSchema);
