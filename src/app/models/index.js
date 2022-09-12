d6a18a95aa364529a68449edf9a14623676630bb
tuantm0303 <minhtuan0330@gmail.com>
2022-09-12 16:47:51 +0700

    feat: user model

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -2,10 +2,12 @@ import productModel from "./product";
 import productOnlineModel from "./productOnlinePrice";
 import productStoreModel from "./productStorePrice";
 import storeModel from "./store";
+import userModel from "./user";
 
 export {
   productModel as Product,
   productOnlineModel as ProductOnline,
   productStoreModel as ProductStore,
   storeModel as Store,
+  userModel as User,
 };
