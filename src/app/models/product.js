698061d8b57827a56d7fd51579bb45ebb958b9a9
tuantm0303 <minhtuan0330@gmail.com>
2022-10-11 16:54:36 +0700

    refactor product

--- src/app/models/product.js
+++ src/app/models/product.js
@@ -2,34 +2,79 @@ import mongoose, { Schema } from "mongoose";
 
 const productSchema = new Schema(
   {
-    name: {
-      type: String,
+    _id: {
+      type: Number,
     },
     brand: {
       type: Number,
+      required: true,
+    },
+    category: {
+      type: Number,
+      required: true,
+    },
+    division: {
+      type: Number,
+      required: true,
+    },
+    function: {
+      type: Number,
+      required: true,
+    },
+    group: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
     },
     parent: {
       type: Number,
+      required: true,
+    },
+    parent_count: {
+      type: Number,
+      required: true,
     },
     quantity: {
       type: Number,
+      required: true,
     },
     sku: {
       type: String,
+      required: true,
+    },
+    source: {
+      type: Number,
+      required: true,
     },
     status: {
-      type: Number,
+      type: Boolean,
+      required: true,
+    },
+    tags: {
+      type: Array,
     },
     thumbnail: {
       type: String,
+      required: true,
     },
     unit: {
       type: String,
+      required: true,
     },
-    slug: {
+    variant: {
       type: String,
-      lowercase: true,
-      index: true,
+      required: true,
+    },
+    range_review: {
+      type: String,
+      required: true,
+    },
+    bixu: {
+      type: Number,
+      required: true,
     },
   },
   { timestamps: true }
